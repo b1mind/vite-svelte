1919d23a06b1141bd844a73318e96fb3b64a7752
b1mind <brent@1mindsolutions.com>
2021-03-21 01:42:46 -0600

    🐣 start of it all

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import svelte from '@svitejs/vite-plugin-svelte'
+import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte({ preprocess: sveltePreprocess })],
+  server: { port: 9001 },
+})
